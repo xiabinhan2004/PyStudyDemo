2ca0c2400ff9ecec588057fc42da5fb5ffceeca4
Xia Binhan <76243464+xiabinhan2004@users.noreply.github.com>
2023-09-27 03:08:22 +0000

    week2

---
+++ Day02/Test.py
@@ -0,0 +1,16 @@
+a=0b101
+b=0b101
+# print(a&b)
+
+x=complex(2,1)
+y=complex(3,1)
+# print(x*y)
+
+print(divmod(10,3)[1])
+abs(-10)
+print(round(10.5))
+print(round(10.4))
+print(round(10.6))
+print(round(10.61,1))
+print(round(10.61,2))
+max(1,2,3,4,5,6,7,8,9)
